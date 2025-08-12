d614906448fe30e6fb89c389c86d035ffb70cdab
irfan-rg <irfanrgulagundi@gmial.com>
2025-08-13 01:32:50 +0530

    production deployment

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,14 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:5173', 
+    'https://thexostore.vercel.app'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
